fc2658b16d5338421e64dd30eda2b2119b3e41e1
iyanrspian <iyanrispian@outlook.com>
2021-05-27 13:22:59 +0700

    install module

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const moment = require('moment');
 const Tiger = require('./lib/Tiger');
 const Wolf = require('./lib/Wolf');
+
+const date = moment().format("Do MMM YYYY");
 
 const fighting = (tiger, wolf) => {
   if(tiger.strength > wolf.strength) {
@@ -19,3 +22,4 @@ const tiger = new Tiger();
 const wolf = new Wolf();
  
 fighting(tiger, wolf);
+console.log(date);
