2b1a0f4f179c93a8ba843e736593e306154ed2b5
iyanrspian <iyanrispian@outlook.com>
2021-05-27 13:31:48 +0700

    events

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const moment = require('moment');
 const myOdd = require('lodash');
 const Tiger = require('./lib/Tiger');
 const Wolf = require('./lib/Wolf');
+const { EventEmitter } = require('events');
 
 const date = moment().format("Do MMM YYYY");
 const myOddEvenArray = myOdd.partition([1, 2, 3, 4, 5, 6], (n) => n % 2);
+const myEventEmitter = new EventEmitter();
 
 const fighting = (tiger, wolf) => {
   if(tiger.strength > wolf.strength) {
@@ -19,10 +21,26 @@ const fighting = (tiger, wolf) => {
 
   console.log('Tiger and Wolf have same strength');
 }
- 
+
+const makeCoffee = (name) => {
+    console.log(`Kopi ${name} telah dibuat!`);
+};
+
+const makeBill = (price) => {
+    console.log(`Bill sebesar ${price} telah dibuat!`);
+}
+
+const onCoffeeOrderedListener = ({ name, price }) => {
+    makeCoffee(name);
+    makeBill(price);
+}
+
 const tiger = new Tiger();
 const wolf = new Wolf();
- 
+
+myEventEmitter.on('coffee-order', onCoffeeOrderedListener);
+myEventEmitter.emit('coffee-order', { name: 'Tubruk', price: 15000 });
+
 fighting(tiger, wolf);
-console.log(myOddEvenArray);
-console.log(date);
+// console.log(myOddEvenArray);
+// console.log(date);
