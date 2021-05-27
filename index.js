d23a684e5c625a6bd51dd2f8b90385fd95a5943f
iyanrspian <iyanrispian@outlook.com>
2021-05-27 13:16:50 +0700

    latihan modularisasi

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-const message = (name) => {
-    console.log(`Hello World! ${name}`);
-}
+const Tiger = require('./lib/Tiger');
+const Wolf = require('./lib/Wolf');
 
-message('Impian');
+const fighting = (tiger, wolf) => {
+  if(tiger.strength > wolf.strength) {
+    tiger.growl();
+    return;
+  }
+
+  if(wolf.strength > tiger.strength) {
+    wolf.howl();
+    return;
+  }
+
+  console.log('Tiger and Wolf have same strength');
+}
+ 
+const tiger = new Tiger();
+const wolf = new Wolf();
+ 
+fighting(tiger, wolf);
